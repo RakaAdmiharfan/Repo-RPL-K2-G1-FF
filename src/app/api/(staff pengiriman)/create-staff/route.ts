8c3feeadfdab6c19cf1a9677a90742ca7ce28ad9
auvarifqi <88789984+auvarifqi@users.noreply.github.com>
2023-11-23 20:46:06 +0700

    update create staff

--- src/app/api/(staff pengiriman)/create-staff/route.ts
+++ src/app/api/(staff pengiriman)/create-staff/route.ts
@@ -1,23 +1,46 @@
 import prisma from "@/app/lib/prismadb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { hash } from "bcryptjs";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { MdReduceCapacity } from "react-icons/md";
 
 
-export async function POST(req: any, res: any) {
-    const { username, password, nama, tanggalLahir, noTelp, alamat, role } = req.body;
+// export async function POST(req: any, res: any) {
+//     const { username, password, nama, tanggalLahir, noTelp, alamat, role } = req.json;
   
-    // Buat staff baru
-    const newStaff = await prisma.user.create({
-      data: {
-        username,
-        password,
-        nama,
-        tanggalLahir: new Date(tanggalLahir),
-        noTelp,
-        alamat,
-        role,
-      },
-    });
-  }
+//     // Buat staff baru
+//     const newStaff = await prisma.user.create({
+//       data: {
+//         username,
+//         password,
+//         nama,
+//         tanggalLahir,
+//         noTelp,
+//         alamat,
+//         role,
+//       },
+//     });
+//   }
+
+export async function POST (req: NextRequest){
+    const { username, password, nama, tanggalLahir, noTelp, alamat, role } = await req.json();
+
+    const session = await getServerSession(authOptions);
+
+    const hashedPassword = await hash(password, 10);
+
+    await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword,
+            nama,
+            tanggalLahir,
+            noTelp,
+            alamat,
+            role,
+        },
+      });
+
+      return NextResponse.json({ message: "success" });
+}
