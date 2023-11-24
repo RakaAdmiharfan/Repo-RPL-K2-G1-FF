71f8f6ee7357c8f5c76ec26f8c384954f613922a
RakaAF123 <rakaadmiharfan@gmail.com>
2023-11-24 17:36:29 +0700

    button mark as read

--- src/app/(pages)/(require login)/notification/page.tsx
+++ src/app/(pages)/(require login)/notification/page.tsx
@@ -7,9 +7,7 @@ import { redirect } from "next/navigation";
 import { UserSession } from "@/components/UserFetcher";
 import client from "@/app/lib/prismadb";
 
-
-
-export default async function notification(){
+export default async function notification() {
   const session = await getServerSession(authOptions);
   const user = session?.user as UserSession;
   const id = user.id;
@@ -27,17 +25,16 @@ export default async function notification(){
         <h1 className="flex justify-center items-center text-[#3D688E] font-montserrat text-2xl lg:text-[48px] font-bold leading-normal tracking-wide mb-[30px] mt-[108px] lg:mt-[52px]">
           Notification
         </h1>
-        <div className="h-3/4 overflow-auto no-scrollbar">
+        <div className="overflow-auto no-scrollbar">
           <div className="overflow-auto">
-            <NotificationBox
-            
-            
-            />
-
-            
+            <NotificationBox />
           </div>
         </div>
-
+        <button className="bg-green-800 px-4 py-1 lg:px-[26px] lg:py-[6px] rounded-[25px] flex items-center justify-center mt-8">
+          <h2 className="text-white font-semibold text-[10px] lg:text-[16px]">
+            Mark As Read
+          </h2>
+        </button>
         <footer className="mt-[140px] lg:mt-[200px] overflow-hidden absolute -bottom-20 lg:bottom-[-340px]">
           <img src="/Footer.png" className="lg:w-[1620px]" />
         </footer>
